67a31bbde29c2295dc60f94ad097051e0939647c
DEVichael <156222302+DEVichael@users.noreply.github.com>
2026-01-18 10:46:57 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,71 @@
+import './App.css';
+import {useEffect, useState} from "react";
+import "milligram";
+import MovieForm from "./MovieForm";
+import MoviesList from "./MoviesList";
+
+function App() {
+    const [movies, setMovies] = useState([]);
+    const [addingMovie, setAddingMovie] = useState(false);
+
+    useEffect(() => {
+
+    const fetchMovies = async () => {
+
+        const response = await fetch(`/movies`);
+
+        if (response.ok) {
+
+            const movies = await response.json();
+
+            setMovies(movies);
+
+        }
+
+    };
+
+    fetchMovies();
+
+}, []);
+
+    async function handleAddMovie(movie) {
+        movie.actors = ''
+
+        const response = await fetch('/movies', {
+
+            method: 'POST',
+
+            body: JSON.stringify(movie),
+
+            headers: {'Content-Type': 'application/json'}
+
+        });
+
+        if (response.ok) {
+
+            setMovies([...movies, movie]);
+
+            setAddingMovie(false);
+
+        }
+
+    }
+
+    return (
+        <div className="container">
+            <h1>My favourite movies to watch</h1>
+            {movies.length === 0
+                ? <p>No movies yet. Maybe add something?</p>
+                : <MoviesList movies={movies}
+                              onDeleteMovie={(movie) => setMovies(movies.filter(m => m !== movie))}
+                />}
+            {addingMovie
+                ? <MovieForm onMovieSubmit={handleAddMovie}
+                             buttonLabel="Add a movie"
+                />
+                : <button onClick={() => setAddingMovie(true)}>Add a movie</button>}
+        </div>
+    );
+}
+
+export default App;
